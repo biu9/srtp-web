410545aa03365c81dc0869d13148642e95fc2916
thy <1756127061@qq.com>
2023-05-01 18:20:10 +0800

    feat: support random wav&img verify

--- components/VerifyModal.js
+++ components/VerifyModal.js
@@ -1,19 +1,20 @@
 import Modal from '@mui/material/Modal'
-import { useState,useEffect,useReducer } from 'react'
-import imgcaptchaAnswer from '/public/lib/pos/1.json'
+import { useState, useEffect, useReducer } from 'react'
+
 
 const CLICK_POINT_RADIUS = 20; // 圆点直径
 const CLICK_POINT_THRESHOLD = 50; // 点击点的误差阈值
+const IMG_NUM = 8; // 图片数量
 
-const clickPointsReducer = (state,action) => {
-  switch(action.type) {
+const clickPointsReducer = (state, action) => {
+  switch (action.type) {
     case 'ADD_CLICK_POINT':
       return [
         ...state,
         {
-          x:action.payload.x,
-          y:action.payload.y,
-          id:action.payload.x
+          x: action.payload.x,
+          y: action.payload.y,
+          id: action.payload.x
         }
       ];
     case 'REMOVE_CLICK_POINT':
@@ -25,7 +26,7 @@ const clickPointsReducer = (state,action) => {
   }
 }
 
-const ClickPoint = ({ x,y,id,onClick }) => {
+const ClickPoint = ({ x, y, id, onClick }) => {
   return (
     <div
       key={id}
@@ -45,8 +46,8 @@ const ClickPoint = ({ x,y,id,onClick }) => {
   )
 }
 
-const handleClick = (event,dispatch) => {
-  const { clientX,clientY } = event;
+const handleClick = (event, dispatch) => {
+  const { clientX, clientY } = event;
   const parentRect = event.target.getBoundingClientRect();
   const res = {
     x: clientX - parentRect.left,
@@ -56,14 +57,24 @@ const handleClick = (event,dispatch) => {
   dispatch({ type: 'ADD_CLICK_POINT', payload: res });
 }
 
-const VerifyModal = ({ ifOpen, setModalOpen,setPass }) => {
-  const [clickPoints,dispatch] = useReducer(clickPointsReducer,[]);
+const VerifyModal = ({ ifOpen, setModalOpen, setPass }) => {
+  const [clickPoints, dispatch] = useReducer(clickPointsReducer, []);
+  const [imgcaptchaAnswer, setImgcaptchaAnswer] = useState(null);
+  const randomIndex = Math.floor(Math.random() * IMG_NUM);
+  const IMG_URL = `/lib/images/${randomIndex}.png`;
+  const WAV_URL = `/lib/wavs/${randomIndex}.wav`;
 
   useEffect(() => {
-    if(clickPoints.length === 4) {
+    fetch(`/lib/pos/${randomIndex}.json`).then(res => res.json()).then(res => {
+      setImgcaptchaAnswer(res)
+    })
+  }, [])
+
+  useEffect(() => {
+    if (clickPoints.length === 4) {
       handleVerify()
     }
-  },[clickPoints])
+  }, [clickPoints])
 
   const onClose = () => {
     setModalOpen(false)
@@ -71,53 +82,53 @@ const VerifyModal = ({ ifOpen, setModalOpen,setPass }) => {
 
   const handleVerify = async () => {
     const pos = imgcaptchaAnswer.answer;
-    console.log('clickPoints',clickPoints)
-    console.log('pos',pos)
-    
-   // 判断四个点是否与预设的点相近
-    for(let i=0;i<pos.length;i++) {
-      if(Math.abs(pos[i].x - clickPoints[i].x) > CLICK_POINT_THRESHOLD || Math.abs(pos[i].y - clickPoints[i].y) > CLICK_POINT_THRESHOLD) {
-        console.log('incorrect key',i)
-        console.log('x diff',Math.abs(pos[i].x - clickPoints[i].x))
-        console.log('y diff',Math.abs(pos[i].y - clickPoints[i].y))
+    console.log('clickPoints', clickPoints)
+    console.log('pos', pos)
+
+    // 判断四个点是否与预设的点相近
+    for (let i = 0; i < pos.length; i++) {
+      if (Math.abs(pos[i].x - clickPoints[i].x) > CLICK_POINT_THRESHOLD || Math.abs(pos[i].y - clickPoints[i].y) > CLICK_POINT_THRESHOLD) {
+        console.log('incorrect key', i)
+        console.log('x diff', Math.abs(pos[i].x - clickPoints[i].x))
+        console.log('y diff', Math.abs(pos[i].y - clickPoints[i].y))
         alert('验证失败');
         setPass(false);
         onClose();
-        dispatch({type:'REMOVE_ALL_CLICK_POINTS'})
+        dispatch({ type: 'REMOVE_ALL_CLICK_POINTS' })
         return null;
       }
     }
     alert('验证成功')
     onClose();
     setPass(true);
-    dispatch({type:'REMOVE_ALL_CLICK_POINTS'})
+    dispatch({ type: 'REMOVE_ALL_CLICK_POINTS' })
   }
-  
+
   return (
-    <Modal 
-    onClose={onClose}
-    open={ifOpen}>
+    <Modal
+      onClose={onClose}
+      open={ifOpen}>
       <div className="modal flex flex-col space-y-3">
-        <div className='text-xl font-bold'>验证你的身份</div> 
+        <div className='text-xl font-bold'>验证你的身份</div>
         <div className='text-sm'> 按顺序点击你听到的词语 </div>
-        <div 
-        onClick={(e) => {
-          handleClick(e,dispatch)
-        }}
-        style={{backgroundImage:`url(${'/lib/images/1.png'})`}} className='w-80 h-80 bg-contain relative'>
+        <div
+          onClick={(e) => {
+            handleClick(e, dispatch)
+          }}
+          style={{ backgroundImage: `url(${IMG_URL})` }} className='w-80 h-80 bg-contain relative'>
           {
             clickPoints.map(point => {
               return (
                 <ClickPoint
                   key={point.id}
-                  x = {point.x}
-                  y = {point.y}
-                  id = {point.id}
+                  x={point.x}
+                  y={point.y}
+                  id={point.id}
                   onClick={(e) => {
                     dispatch({
-                      type:'REMOVE_CLICK_POINT',
-                      payload:{
-                        id:point.id
+                      type: 'REMOVE_CLICK_POINT',
+                      payload: {
+                        id: point.id
                       }
                     })
                     e.stopPropagation();
@@ -127,7 +138,7 @@ const VerifyModal = ({ ifOpen, setModalOpen,setPass }) => {
             })
           }
         </div>
-        <audio src="/lib/wavs/1.wav" controls></audio>
+        <audio src={WAV_URL} controls></audio>
       </div>
     </Modal>
   )
